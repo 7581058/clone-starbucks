dc158dd030d2b4663065478d0849430ee84ff6c1
daseul <7581058@gmail.com>
2023-03-28 15:27:45 +0900

    Common module separation

--- js/main.js
+++ js/main.js
@@ -1,21 +1,3 @@
-// search
-const searchEl = document.querySelector('.search')
-const searchInputEl = searchEl.querySelector('input')
-
-searchEl.addEventListener('click', function () {
-  searchInputEl.focus()
-})
-
-searchInputEl.addEventListener('focus', function () {
-  searchEl.classList.add('focused')
-  searchInputEl.setAttribute('placeholder', '통합검색')
-})
-
-searchInputEl.addEventListener('blur', function () {
-  searchEl.classList.remove('focused')
-  searchInputEl.setAttribute('placeholder', '')
-})
-
 // badges 
 const badgeEl  = document.querySelector('header .badges')
 
@@ -138,6 +120,3 @@ spyEls.forEach(function (spyEl) {
     .addTo(new ScrollMagic.Controller())
 })
 
-// year
-const thisYear = document.querySelector('.this-year')
-thisYear.textContent = new Date().getFullYear()
